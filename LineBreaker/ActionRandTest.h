9691ff3dd8f52fa6c1af6743c25f5513008a140a
FalcoGer <paul_meckel@web.de>
2018-06-29 23:01:21 +0200

    * FIXME calling static function

--- LineBreaker/ActionRandTest.h
+++ LineBreaker/ActionRandTest.h
@@ -14,7 +14,14 @@ public:
     inline ActionRandTest(GameState* gs, float min, float max) : GameAction(gs) { this->min = min; this->max = max; }
     inline ~ActionRandTest() {}
 
+    // FIXME
     // inline void invoke() { cout << Framework::getRandomFloat(min, max) << endl; }
+    // Code     Beschreibung                                                                                    Zeile
+    // C2027    Verwendung des undefinierten Typs "Framework" (Quelldatei wird kompiliert main.cpp)             17
+    // C3861    "getRandomFloat": Bezeichner wurde nicht gefunden. (Quelldatei wird kompiliert main.cpp)        17
+    // C2027    Verwendung des undefinierten Typs "Framework" (Quelldatei wird kompiliert Framework.cpp)        17
+    // C3861    "getRandomFloat": Bezeichner wurde nicht gefunden. (Quelldatei wird kompiliert Framework.cpp)   17
+
     inline void invoke() { cout << "Fixme" << endl; }
 private:
     float min;
